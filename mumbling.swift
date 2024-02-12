37c3cbdafc78d641ef423cc3a5353109716202f8
Jaliel <kangjaliel1998@gmail.com>
2024-02-12 10:58:10 +0700

    mumbling

--- mumbling.swift
+++ mumbling.swift
@@ -1,5 +1,14 @@
-for _ in 1...3 {
-    print("Hello")
+func accum(_ s: String) -> String {
+
+  var results = ""
+  for (index, char) in s.enumerated() {
+    let repeatedChar = String(repeating: char, count: index)
+    results += "\(char.uppercased())\(repeatedChar.lowercased())-"
+  }
+  return String(results.dropLast())
 }
 
-print("Hello")
+//OUTPUT
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
