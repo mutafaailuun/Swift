b3095f46fe966bd6eff4b5c37a86cc1950b2178a
Jaliel <kangjaliel1998@gmail.com>
2024-02-15 09:49:18 +0700

    minus

--- functional-addition.swift
+++ functional-addition.swift
@@ -4,3 +4,13 @@ func add(_ n: Int) -> ((Int) -> Int) {
 
 var addOne = add(1)
 print(addOne(5))
+//easy understanding way
+
+func reduce(_ n: Int) -> ((Int) -> Int) {
+  return { x in
+    x - n
+  }
+}
+
+var minusOne = reduce(1)
+print(minusOne(5))
